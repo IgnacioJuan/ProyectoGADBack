042413e6c5a99bac3f23933ea8d29ab7a0b6bf87
Christian Crespo <110556053+ChristianC81@users.noreply.github.com>
2023-08-28 12:11:47 -0500

    Ajustes ods beta

--- src/main/java/com/sistema/examenes/repository/ObjetivoODSRepository.java
+++ src/main/java/com/sistema/examenes/repository/ObjetivoODSRepository.java
@@ -9,8 +9,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ObjetivoODSRepository extends JpaRepository<ObjetivoODS, Long> {
+    
     @Query(value = "SELECT * from objetivoods where visible =true ORDER BY nombre ASC", nativeQuery = true)
     List<ObjetivoODS> listarObjetivosODS();
+   
     @Query(value = "SELECT id_objetivo_ods, nombre, descripcion FROM objetivo_ods WHERE LOWER(nombre) LIKE LOWER(CONCAT('%', :nombre, '%')) AND visible = true", nativeQuery = true)
     List<Object[]> buscarObjetivosODSPorNombre(@Param("nombre") String nombre);
 
