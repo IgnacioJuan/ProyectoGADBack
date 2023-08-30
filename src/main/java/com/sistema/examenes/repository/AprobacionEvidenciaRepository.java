0ab03f58062c0127b0fac4602b8a6a4de3738251
dianasaico20 <diana.saico.est@tecazuay.edu.ec>
2023-08-30 16:30:05 -0500

    Add  servicios aprobacion evidencia

---
+++ src/main/java/com/sistema/examenes/repository/AprobacionEvidenciaRepository.java
@@ -0,0 +1,12 @@
+package com.sistema.examenes.repository;
+
+import com.sistema.examenes.entity.AprobacionEvidencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AprobacionEvidenciaRepository extends JpaRepository<AprobacionEvidencia, Long> {
+    @Query(value = "SELECT * from aprobacion_evidencia where visible = true", nativeQuery = true)
+    List<AprobacionEvidencia> listarAprobacionEvidencia();
+}
