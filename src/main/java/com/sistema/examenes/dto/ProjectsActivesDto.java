a78e3e84b60680c035acffd580e0d7f040938e0b
dverao1997 <diego.r.vera.ortiz@gmail.com>
2023-09-01 12:28:57 -0500

    ptimer commit

---
+++ src/main/java/com/sistema/examenes/dto/ProjectsActivesDto.java
@@ -0,0 +1,4 @@
+package com.sistema.examenes.dto;
+
+public class ProjectsActivesDto {
+}
--- src/main/java/com/sistema/examenes/repository/ProyectoRepository.java
+++ src/main/java/com/sistema/examenes/repository/ProyectoRepository.java
@@ -11,4 +11,6 @@ public interface ProyectoRepository extends JpaRepository<Proyecto, Long> {
     List<Proyecto> listarProyectos();
     @Query(value = "SELECT * from proyecto where visible =true and id_modelo_poa = :id_modelo_poa ORDER BY nombre ASC ", nativeQuery = true)
     List<Proyecto> listarProyectosdelModelo(Long id_modelo_poa);
+    @Query(value = "SELECT p.id_proyecto,p.codigo,p.meta,p.nombre FROM public.proyecto p join public.modelopoa ON modelopoa.id_modelo_poa = p.id_modelo_poa  WHERE modelopoa.estado='ACTIVO' AND p.visible=true ORDER BY p.nombre", nativeQuery = true)
+    List<Object[]> listProjectsActives();
 }
