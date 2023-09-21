eaf9666aacb7425a558ac6ded0260c222b33ec40
Jhudyt Gaona <jhudyt.gaona.est@tecazuay.edu.ec>
2023-09-20 19:18:00 -0500

    fix userResponsable filtrado por programaULogeado

--- src/main/java/com/sistema/examenes/repository/auth/UsuarioRolRepository.java
+++ src/main/java/com/sistema/examenes/repository/auth/UsuarioRolRepository.java
@@ -10,47 +10,47 @@ import java.util.List;
 
 public interface UsuarioRolRepository extends JpaRepository<UsuarioRol, Long> {
 
-    @Query("SELECT ur FROM UsuarioRol ur JOIN FETCH ur.usuario u WHERE u.visible = true")
-    List<UsuarioRol> listarv();
+        @Query("SELECT ur FROM UsuarioRol ur JOIN FETCH ur.usuario u WHERE u.visible = true")
+        List<UsuarioRol> listarv();
 
-    UsuarioRol findByUsuario_Id(Long usuarioId);
+        UsuarioRol findByUsuario_Id(Long usuarioId);
 
-    //LISTAR USUARIOS RESPONSABLES (TODOS)
-    @Query("SELECT ur FROM UsuarioRol ur JOIN FETCH ur.usuario u WHERE u.visible = true AND ur.rol.rolId = 3")
-    List<UsuarioRol> listarUsuariosResponsables();
-    @Query("SELECT ur FROM UsuarioRol ur JOIN FETCH ur.usuario u WHERE u.visible = true AND ur.rol.rolId = 1")
-    List<UsuarioRol> listarUsuariosSuperAdmin();
+        // LISTAR USUARIOS RESPONSABLES (TODOS)
+        @Query("SELECT ur FROM UsuarioRol ur JOIN FETCH ur.usuario u WHERE u.visible = true AND ur.rol.rolId = 3")
+        List<UsuarioRol> listarUsuariosResponsables();
 
-    @Query(value = "SELECT " +
-            "    u.id AS id_usuario_responsable, " +
-            "    p.primer_nombre, " +
-            "    p.segundo_nombre, " +
-            "    p.primer_apellido, " +
-            "    p.segundo_apellido, " +
-            "    u.username AS usuario, " +
-            "    prog.nombre AS programa, " +
-            "    p.cargo " +
-            "FROM public.usuariorol ur " +
-            "JOIN public.usuarios u ON ur.usuario_id = u.id " +
-            "JOIN public.persona p ON u.persona_id_persona = p.id_persona " +
-            "JOIN public.programa prog ON u.id_programa = prog.id_programa " +
-            "WHERE ur.rol_rolid = 3 AND u.visible = true", nativeQuery = true)
-    List<Object[]> listarUResponsables();
+        @Query("SELECT ur FROM UsuarioRol ur JOIN FETCH ur.usuario u WHERE u.visible = true AND ur.rol.rolId = 1")
+        List<UsuarioRol> listarUsuariosSuperAdmin();
 
-    //LISTAR USUARIOS RESPONSABLES, POR PROGRAMA
-    @Query(value = "SELECT ur FROM UsuarioRol ur " +
-            "JOIN FETCH ur.usuario u " +
-            "JOIN FETCH u.programa p " +
-            "WHERE u.visible = true " +
-            "AND ur.rol.rolId = 3 " + // Filtrar por rol "RESPONSABLE"
-            "AND p.id_programa = (" +
-            "  SELECT pro.programa.id_programa FROM Proyecto pro " +
-            "  WHERE pro.id_proyecto = (" +
-            "    SELECT poa.proyecto.id_proyecto FROM Poa poa " +
-            "    WHERE poa.id_poa = :poaId" + // Obtener proyecto del POA
-            "  )" +
-            ")")
-    List<UsuarioRol> listarUsuariosResponsables2(@Param("poaId") Long poaId);
+        @Query(value = "SELECT " +
+                        "    u.id AS id_usuario_responsable, " +
+                        "    p.primer_nombre, " +
+                        "    p.segundo_nombre, " +
+                        "    p.primer_apellido, " +
+                        "    p.segundo_apellido, " +
+                        "    u.username AS usuario, " +
+                        "    prog.nombre AS programa, " +
+                        "    p.cargo " +
+                        "FROM public.usuariorol ur " +
+                        "JOIN public.usuarios u ON ur.usuario_id = u.id " +
+                        "JOIN public.persona p ON u.persona_id_persona = p.id_persona " +
+                        "JOIN public.programa prog ON u.id_programa = prog.id_programa " +
+                        "WHERE ur.rol_rolid = 3 AND u.visible = true AND u.id_programa = :programaUsuarioLogeado", nativeQuery = true)
+        List<Object[]> listarUResponsables(@Param("programaUsuarioLogeado") Long programaUsuarioLogeado);
 
+        // LISTAR USUARIOS RESPONSABLES, POR PROGRAMA
+        @Query(value = "SELECT ur FROM UsuarioRol ur " +
+                        "JOIN FETCH ur.usuario u " +
+                        "JOIN FETCH u.programa p " +
+                        "WHERE u.visible = true " +
+                        "AND ur.rol.rolId = 3 " + // Filtrar por rol "RESPONSABLE"
+                        "AND p.id_programa = (" +
+                        "  SELECT pro.programa.id_programa FROM Proyecto pro " +
+                        "  WHERE pro.id_proyecto = (" +
+                        "    SELECT poa.proyecto.id_proyecto FROM Poa poa " +
+                        "    WHERE poa.id_poa = :poaId" + // Obtener proyecto del POA
+                        "  )" +
+                        ")")
+        List<UsuarioRol> listarUsuariosResponsables2(@Param("poaId") Long poaId);
 
 }
