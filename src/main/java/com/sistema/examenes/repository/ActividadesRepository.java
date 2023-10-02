5a1e31f591f4672372e1aa2560044961cd91923b
Miriam Medina <miriam.medina.est@tecazuay.edu.ec>
2023-10-02 12:11:41 -0500

    correcciones Dtos

--- src/main/java/com/sistema/examenes/repository/ActividadesRepository.java
+++ src/main/java/com/sistema/examenes/repository/ActividadesRepository.java
@@ -1,5 +1,6 @@
 package com.sistema.examenes.repository;
 
+import com.sistema.examenes.dto.ActividadDTO;
 import com.sistema.examenes.entity.Actividades;
 import com.sistema.examenes.projection.ActividadesPendientesPorPoaProjection;
 import com.sistema.examenes.projection.valorprojec;
@@ -16,9 +17,16 @@ public interface ActividadesRepository extends JpaRepository<Actividades, Long> 
         @Query(value = "SELECT * from actividades where visible = true ORDER BY nombre ASC", nativeQuery = true)
         List<Actividades> listarActividades();
 
+        @Query(value = "SELECT id_actividad, nombre, descripcion, presupuesto_referencial, recursos_propios, codificado, devengado, estado, id_poa from actividades where visible = true ORDER BY nombre ASC", nativeQuery = true)
+        List<Object[]> listarActividades2();
 
+
+        @Query(value = "SELECT a.id_actividad, a.nombre, a.descripcion, a.presupuesto_referencial, a.recursos_propios, a.codificado, a.devengado, a.estado FROM actividades a JOIN poa p ON a.id_poa = p.id_poa WHERE a.visible = true AND p.id_poa = :poaId ORDER BY a.nombre ASC", nativeQuery = true)
+        List<Object[]> listarActividadesPorIdPoa(@Param("poaId") Long poaId);
+
+        //Para chris
         @Query(value = "SELECT * FROM actividades a JOIN poa p ON a.id_poa = p.id_poa WHERE a.visible = true AND p.id_poa = :poaId ORDER BY a.nombre ASC", nativeQuery = true)
-        List<Actividades> listarActividadesPorIdPoa(@Param("poaId") Long poaId);
+        List<Actividades> listarActividadesPorIdPoa2(@Param("poaId") Long poaId);
 
         /*
          * @Query(value =
